6cbb262adb67dab84d6c33d485b1e03e81ed7405
low-level-mastery <obilev00@gmail.com>
2026-06-24 09:29:45 +0500

    feat: Add header file for video module and input module

---
+++ include/rk/input.h
@@ -0,0 +1,54 @@
+#ifndef RK_INPUT_H
+#define RK_INPUT_H
+
+#include <stdbool.h>
+
+struct rk_input_instance;
+
+enum rk_input_mouse_button {
+	RK_INPUT_LEFT_BUTTON,
+	RK_INPUT_RIGHT_BUTTON
+};
+
+/**
+ * rk_input_handle - Update event list
+ * @inst: Instance pointer
+ * 
+ * This function conducts a survey of states
+ * and save it to rk_input_instance.keyboard_state
+ *
+ * Also this function get cursor pos and save it
+ * to rk_input_instance.cursor.x/y
+*/
+void rk_input_handle(struct rk_input_instance *inst);
+
+/**
+ * rk_input_get_mouse - Get mouse position in window
+ * @inst: Instance pointer
+ * 
+ * This function returns mouse position from @inst
+*/
+float *rk_input_get_mouse(struct rk_input_instance *inst);
+
+/**
+ * rk_input_is_mouse_pressed - Check mouse button state
+ * @inst: Instance pointer
+ * @btn: Button side
+ * 
+ * This function check mouse button state
+ * Return true if @btn side is pressed, else false
+*/
+bool rk_input_is_mouse_pressed(struct rk_input_instance *inst,
+			       enum rk_input_mouse_button btn);
+
+/**
+ * rk_input_is_key_pressed - Check is key pressed
+ * @inst: Instance pointer
+ * @key: Key for check state
+ * 
+ * This function check key state
+ * Returns key state, true - pressed, else false
+*/
+bool rk_input_is_key_pressed(struct rk_input_instance *inst, char key);
+
+#endif /* RK_INPUT_H */
---
+++ include/rk/video.h
@@ -0,0 +1,83 @@
+#ifndef RK_VIDEO_H
+#define RK_VIDEO_H
+
+#include <SDL3/SDL.h>
+
+struct rk_video_instance;
+
+/**
+ * rk_video_alloc - Allocates memory for instance
+ * 
+ * This function allocates memory by system allocator
+ * for instance
+*/
+struct rk_video_instance *rk_video_alloc(void);
+
+/**
+ * rk_video_init - Initialize instance
+ * @inst: Instance pointer
+ * @title: Window title
+ * @width: Window width
+ * @height: Window height
+ * 
+ * This function initialize video module
+ * Return 0 if success or negative error code if fail
+*/
+int rk_video_init(struct rk_video_instance *inst, const char *title, int width,
+		  int height);
+
+/**
+ * rk_video_ctx_clear - Clear window context
+ * @inst: Instance poiner
+ *
+ * This function clear window context by current active color
+*/
+void rk_video_ctx_clear(struct rk_video_instance *inst);
+
+/**
+ * rk_video_ctx_swap - Swap context buffers
+ * @inst: Instance pointer
+ *
+ * This function swap context buffers and present changes
+*/
+void rk_video_ctx_swap(struct rk_video_instance *inst);
+
+/**
+ * rk_video_ctx_use_color - Change draw color
+ * @inst: Instance pointer
+ * @r: Red ratio in color
+ * @g: Green ratio in color
+ * @b: Blue ratio in color
+ * @a: Alpha number
+ * 
+ * This function set color for draw
+ * 
+ * Important!
+ * Color sets in normalized type (-1.0f - 1.0f)
+*/
+void rk_video_ctx_use_color(struct rk_video_instance *inst, float r, float g,
+			    float b, float a);
+
+/**
+ * rk_video_ctx_add_line - Add line to context buffer
+ * @inst: Instance pointer
+ * @start_x: Line start x coord
+ * @start_y: Line start y coord
+ * @end_x: Line end x coord
+ * @end_y: Line end y coord
+ * 
+ * This function add line to context buffer. For see changes
+ * use rk_video_ctx_swap function
+*/
+void rk_video_ctx_add_line(struct rk_video_instance *inst, float start_x,
+			   float start_y, float end_x, float end_y);
+
+/**
+ * rk_video_free - Free all module resources
+ * @inst: Instance pointer
+ * 
+ * This function free all resources who allocated/used
+*/
+void rk_video_free(struct rk_video_instance *inst);
+
+#endif /* RK_VIDEO_H */
